25cd06a63614fb2881ba37a539a000d852c333a4
yallo428 <yallo428@naver.com>
2024-04-14 12:59:33 +0900

    :sparkles: member 회원가입 로그인 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardPage from '../pages/DashboardPage';
+import MemberSignInPage from '../pages/MemberSignInPage';
+import MemberSignUpPage from "../pages/MemberSignUpPage";
 import App from '../App';
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
@@ -12,6 +14,8 @@ const Router = () => {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/login" element={<MemberSignInPage/>}/>
+                <Route path="/signup" element={<MemberSignUpPage/>}/>
             </Routes>
             <Footer />
         </BrowserRouter>
