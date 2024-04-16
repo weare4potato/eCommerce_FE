5e6207df4db58707221ec36a30f090b364cd3bbc
yallo428 <116006875+yallo428@users.noreply.github.com>
2024-04-17 00:53:24 +0900

    Revert ":sparkles: member 회원가입 로그인 추가"
    This reverts commit daa3bb11cd592298825f110fdb69a12c16f4ce74.

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardPage from '../pages/DashboardPage';
-import MemberSignInPage from '../pages/MemberSignInPage';
-import MemberSignUpPage from "../pages/MemberSignUpPage";
 import App from '../App';
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
@@ -14,8 +12,6 @@ const Router = () => {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/login" element={<MemberSignInPage/>}/>
-                <Route path="/signup" element={<MemberSignUpPage/>}/>
             </Routes>
             <Footer />
         </BrowserRouter>
